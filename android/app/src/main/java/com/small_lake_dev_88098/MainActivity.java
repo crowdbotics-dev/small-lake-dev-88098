35d9653bbd937243a0dcd363048018f6b4defd92
Crowdbotics <engineering@crowdbotics.com>
2023-06-02 12:30:58 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/small_lake_dev_88098/MainActivity.java
@@ -0,0 +1,15 @@
+package com.small_lake_dev_88098;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "small_lake_dev_88098";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "small-lake-dev-88098",
+  url: "https://small-lake-dev-88098.botics.co",
+  api: "https://small-lake-dev-88098.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
